ce29c849ba4180fd3ebd6faf65eaabfde6562fbf
Yasir Malik <yasirrafiq786@gmail.com>
2020-07-04 21:22:52 +1000

    redid the whole thing

---
+++ index.js
@@ -0,0 +1,7 @@
+// const aboutme = document.getElementById('aboutme');
+
+// aboutme.addEventListener(('click'), () => {
+//     const container = document.querySelector('.page-container');
+//     container.classList.add('content-fade');
+//     container.innerHTML="<object type='text/html' data='pages/aboutme.html'></object>";
+// })
